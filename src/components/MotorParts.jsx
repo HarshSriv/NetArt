b6aae632646f0e1ddeab66f5ecdfb05dbc1afc45
HarshSriv <harshsrivastava041@gmail.com>
2024-02-27 22:56:22 +0530

    new1

--- src/components/MotorParts.jsx
+++ src/components/MotorParts.jsx
@@ -9,7 +9,7 @@ function MotorParts() {
     <div className='motor-parts'>
       <p>INSTALLED OVER 10 LAKHS STAR RATED PUMPSETS ACROSS THE COUNTRY RESULTING IN A CUMULATIVE SAVING OF MORE THAN 9,000 MILLION UNITS OF POWER FOR THE NATION.</p>
        <br/>
-      <img src={image}></img>  
+      <img className="moto" src={image}></img>  
       <br/>
       <p>Valves - Pumps - Pipes - IoT Drives & Controllers - Wires & Cables - Solar Systems - Motors</p><br/>
 
